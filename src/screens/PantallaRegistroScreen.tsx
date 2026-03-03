d712454f341c3013dd4ef66eda97b5738fc8bb7d
gaby <kathy.paucar47@gmail.com>
2026-03-02 19:31:27 -0500

    terminado

--- src/screens/PantallaRegistroScreen.tsx
+++ src/screens/PantallaRegistroScreen.tsx
@@ -1,98 +1,95 @@
 import React, { useState } from "react";
-import {View, Text, TextInput, TouchableOpacity, Alert} from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { stylesGlobal } from "../theme/appTheme";
-import { User } from '../navigator/StackNavigator';
+import { User } from "../navigator/StackNavigator";
 import { CommonActions, useNavigation } from "@react-navigation/native";
 
-
-  interface FormRegister {
-    name: string;
-    lastName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
+interface FormRegister {
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
 
-interface Props{
+interface Props {
   users: User[];
   handleAddUser: (user: User) => void;
 }
 
-export const PantallaRegistroScreen = ({users, handleAddUser}: Props) => {
+export const PantallaRegistroScreen = ({ users, handleAddUser }: Props) => {
   const [formRegister, setFormRegister] = useState<FormRegister>({
-        name: ' ',
-        lastName: ' ',
-        email: ' ',
-        password: ' ',
-        confirmPassword: ' '
-    });
+    name: " ",
+    lastName: " ",
+    email: " ",
+    password: " ",
+    confirmPassword: " ",
+  });
 
-    //hoook useNavigation: permite navegar de una pantalla a otra
-    const navigation = useNavigation();
+  //hoook useNavigation: permite navegar de una pantalla a otra
+  const navigation = useNavigation();
 
-    //funcion que captura los datso del formulario
-    const handleChangeValue = (name: string, value: string): void => {
-        //llamas funcion para actualizar el estado del formulario
-        setFormRegister({...formRegister, [name]: value});
+  //funcion que captura los datso del formulario
+  const handleChangeValue = (name: string, value: string): void => {
+    //llamas funcion para actualizar el estado del formulario
+    setFormRegister({ ...formRegister, [name]: value });
+  };
+
+  //funcion para ver si el usuario existe
+  const verify = () => {
+    const existUser = users.filter(
+      (user) => user.email == formRegister.email,
+    )[0];
+    return existUser;
+  };
+
+  //funcion para generas los nuevos ids de los usuarios
+  const getIdUser = () => {
+    const getId = users.length + 1;
+    return getId;
+  };
+
+  //funcion para registrar a los usuarios
+  const handleRegister = (): void => {
+    const { name, lastName, email, password, confirmPassword } = formRegister;
+    if (!name || !lastName || !email || !password || !confirmPassword) {
+      Alert.alert("Error", "Todos los campos son obligatorios");
+      return;
     }
 
-    //funcion para ver si el usuario existe
-    const verify = () => {
-      const existUser = users.filter(user => user.email == formRegister.email)[0];
-      return existUser;
+    if (verify()) {
+      Alert.alert("Error", "El usuario ya se encuentra registrado");
+      return;
     }
+    //crear objeto user
+    const newUSer: User = {
+      id: getIdUser(),
+      name: formRegister.name,
+      lastName: formRegister.lastName,
+      email: formRegister.email,
+      password: formRegister.password,
+      confirmPassword: formRegister.confirmPassword,
+    };
 
-    //funcion para generas los nuevos ids de los usuarios
-    const getIdUser = () => {
-      const getId = users.length + 1;
-      return getId;
-    }
+    //añadir al arreglo
+    handleAddUser(newUSer);
+    Alert.alert("Bienvenido", "Usuario registrado con exito");
 
-    //funcion para registrar a los usuarios
-    const handleRegister = (): void => {
-      if(formRegister.name == ' ' || formRegister.lastName == ' ' || formRegister.email == ' ' || formRegister.password == ' ' || formRegister.confirmPassword == ' '){
-        Alert.alert("Error", "El usuario ya se encuentra registrado");
-        return;
-      }
-      //crear objeto user
-      const newUSer: User = {
-        id: getIdUser(),
-        name: formRegister.name,
-        lastName: formRegister.lastName,
-        email: formRegister.email,
-        password: formRegister.password,
-        confirmPassword: formRegister.confirmPassword,
-      }
-
-      //añadir al arreglo 
-      handleAddUser (newUSer);
-      Alert.alert ("Bienvenido", "Usuario registrado con exito");
-
-      //redireccionar al login
-      navigation.goBack();
-    }
-
-    //funcion iniciar sesion
-    const handleSignUp = (): void => {
-        const {name, lastName, email, password, confirmPassword} = formRegister
-              if (!name || !lastName || !email || !password || !confirmPassword) {
-              Alert.alert("Error", "Todos los campos son obligatorios");
-              return;
-            }
-        
-            Alert.alert("Login correcto", `Bienvenido ${name}`);
-                console.log(formRegister);
-            }
+    //redireccionar al login
+    navigation.goBack();
+  };
 
   return (
     <View style={stylesGlobal.container}>
       <Text style={stylesGlobal.title}>Regístrate Ahora!</Text>
-      <Text style={stylesGlobal.subtitle}>Y descubre todo lo que tenémos para tí </Text>
+      <Text style={stylesGlobal.subtitle}>
+        Y descubre todo lo que tenémos para tí{" "}
+      </Text>
 
       <TextInput
         placeholder="Nombre"
         keyboardType="default"
-        onChangeText={(value)=>handleChangeValue("name", value)}
+        onChangeText={(value) => handleChangeValue("name", value)}
         style={stylesGlobal.input}
         placeholderTextColor={stylesGlobal.input.color}
       />
@@ -100,14 +97,14 @@ export const PantallaRegistroScreen = ({users, handleAddUser}: Props) => {
       <TextInput
         placeholder="Apellido"
         keyboardType="default"
-        onChangeText={(value)=>handleChangeValue("lastName", value)}
+        onChangeText={(value) => handleChangeValue("lastName", value)}
         style={stylesGlobal.input}
         placeholderTextColor={stylesGlobal.input.color}
       />
 
       <TextInput
         placeholder="Correo electrónico"
-        onChangeText={(value)=>handleChangeValue("email", value)}
+        onChangeText={(value) => handleChangeValue("email", value)}
         style={stylesGlobal.input}
         keyboardType="email-address"
         autoCapitalize="none"
@@ -117,7 +114,7 @@ export const PantallaRegistroScreen = ({users, handleAddUser}: Props) => {
       <TextInput
         placeholder="Contraseña"
         keyboardType="default"
-        onChangeText={(value)=>handleChangeValue("password", value)}
+        onChangeText={(value) => handleChangeValue("password", value)}
         style={stylesGlobal.input}
         placeholderTextColor={stylesGlobal.input.color}
         secureTextEntry
@@ -126,24 +123,27 @@ export const PantallaRegistroScreen = ({users, handleAddUser}: Props) => {
       <TextInput
         placeholder="Confirmar Contraseña"
         keyboardType="default"
-        onChangeText={(value)=>handleChangeValue("confirmPassword", value)}
+        onChangeText={(value) => handleChangeValue("confirmPassword", value)}
         style={stylesGlobal.input}
         placeholderTextColor={stylesGlobal.input.color}
         secureTextEntry
       />
 
-      <TouchableOpacity style={stylesGlobal.button} onPress={handleSignUp}>
+      <TouchableOpacity style={stylesGlobal.button} onPress={handleRegister}>
         <Text style={stylesGlobal.buttonText}>Registrarse</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={()=> 
-        navigation.dispatch(CommonActions.navigate({name: 'PantallaInicioSesion'}))}>
-        <Text style={stylesGlobal.link}
-       >¿Si ya tienes cuenta? Inicia Sesión</Text>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.dispatch(
+            CommonActions.navigate({ name: "PantallaInicioSesion" }),
+          )
+        }
+      >
+        <Text style={stylesGlobal.link}>
+          ¿Si ya tienes cuenta? Inicia Sesión
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-
-
